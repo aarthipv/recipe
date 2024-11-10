fb4304f88f2731537f90a7fe27562164fbc62ee2
aarthipv <aarthipv2004@gmail.com>
2024-11-10 23:35:50 +0530

    final changes

--- server/src/models/Recipes.js
+++ server/src/models/Recipes.js
@@ -1,19 +1,34 @@
 import mongoose from "mongoose";
-const RecipeSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    ingridients:[{
-        type:String,
-        required:true
 
-    }],
-    instructions:{type:String,required:true},
-    imageUrl:{type:String,required:true},
-    cookingTime:{type:Number,required:true},
-    userOwner: {type:mongoose.Schema.Types.ObjectId, ref:"users", required: true}
+const recipeSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  ingredients: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  instructions: {
+    type: String,
+    required: true,
+  },
+
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  cookingTime: {
+    type: Number,
+    required: true,
+  },
+  userOwner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
-
-export const RecipeModel=mongoose.model("recipes",RecipeSchema)
+export const RecipesModel = mongoose.model("Recipes", recipeSchema);
